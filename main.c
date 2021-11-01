c3cac0ee896a4c52dda59a365feaa5b704eb8fed
WIN-94H50OFE5L9\User <bayarto2003@gmail.com>
2021-11-01 16:30:12 +0700

    test

--- main.c
+++ main.c
@@ -53,16 +53,4 @@ void arr_output(int *arr, int size) {
     fclose(f_out);
 }
 
-
-int main() {
-    int size;
-    FILE *f_in = fopen("in.txt", "r");
-    size_input(&size, f_in);
-    // Allocate memory for the array and fill it.
-    int *arr = malloc(sizeof(int) * size);
-    fill_arr(arr, size, f_in);
-    fclose(f_in);
-    quick_sort(arr, 0, size - 1);
-    arr_output(arr, size);
-    return 0;
 }
